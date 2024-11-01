d3256b17f1f21e1376b0b8d9e08c0ff8d5bada4c
Mukund <mukundlokhande926@gmail.com>
2024-11-01 10:00:47 +0530

    First Commit

---
+++ DailyDoseOfJava/Patterns/com/Patterns/IncrementalAlphabetHalfPyramid.java
@@ -0,0 +1,18 @@
+package com.Patterns;
+
+public class IncrementalAlphabetHalfPyramid 
+{
+	public static void main (String[] args)
+    {
+        char character = 'A';
+         for(int i = 1; i <= 4; i++)
+         {
+            for (int j = 1; j <= i; j++)
+            {
+                System.out.print(character +" ");
+                character++;
+            }
+            System.out.println();
+         }
+    }
+}
